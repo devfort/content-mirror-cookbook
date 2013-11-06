dbe9e0c18ee015688989493ddd7e2738998fd658
Steve Marshall <steve@nascentguruism.com>
2013-11-06 16:30:37 +0000

    Attributify remote host, tidy command.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['cpan_mirror']['user'] = 'fort'
 default['cpan_mirror']['data_dir'] = '/data/cpan'
+default['cpan_mirror']['remote'] = 'cpan-rsync.perl.org::CPAN'
 
 default['cpan_mirror']['apache']['listen_hostname'] = 'cpan.fort'
 default['cpan_mirror']['apache']['listen_port'] = '80'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,6 @@ mirror "cpan" do
   user node.cpan_mirror.user
   hostname node.cpan_mirror.apache.listen_hostname
   port node.cpan_mirror.apache.listen_port
-  command "rsync -av --delete cpan-rsync.perl.org::CPAN #{node.cpan_mirror.data_dir}"
+  command "rsync -aP --delete #{node.cpan_mirror.remote} #{node.cpan_mirror.data_dir}"
   cookbook "mirror-core"
 end
